2be2d9daedf9f0017d644e219c9baedba7f646d6
Bruno dos  Santos Silva <83733535+BrunoDosSantosSilva@users.noreply.github.com>
2021-07-07 18:47:22 -0300

    a

--- templates/index.php
+++ templates/index.php
@@ -1,10 +1,3 @@
 <?php
   include_once("index.html");
-  include_once("create.html");
-  include_once("delete.html");
-  include_once("header.html");
-  include_once("header2.html");
-  include_once("read_all.html");
-  include_once("read_single.html");
-  include_once("update.html");
 ?>
